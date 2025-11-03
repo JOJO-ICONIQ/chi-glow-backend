241b2b9cc52a1ddad9d7b8616d158fd460be9561
JOJO-ICONIQ <jojo.image@icloud.com>
2025-11-03 03:14:53 -0500

    Add Firebase configuration to index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+let firebaseConfig = {
+  apiKey: "AIzaSyB0dUMMYFAKEKEY1234567890", // We'll gen real one in a sec
+  authDomain: "chi-glow-money.firebaseapp.com",
+  projectId: "chi-glow-money",
+  storageBucket: "chi-glow-money.appspot.com",
+  messagingSenderId: "78932978337",
+  appId: "1:78932978337:web:abcdef123456"
+};
